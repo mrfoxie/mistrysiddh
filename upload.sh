e0216a343be5070ebe63dcff9528f993ef02ea7f
mrfoxie <siddh.mistry396321@gmail.com>
2021-01-24 22:57:03 +0530

    Talks updated

--- upload.sh
+++ upload.sh
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-git add .
-read -p "Enter commit: " commit
-git commit -m "$commit"
-git push
-=======
 git add . --verbose
 read -p "Enter commit: " commit
 git commit -m "$commit" --verbose
 git push --verbose
->>>>>>> cd8cdf223af785b3b334598e31903f910bd5433b
